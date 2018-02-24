f58def2505294045c72b37449d826bc297196b80
troyready <troy@troyready.com>
2018-02-23 17:50:41 -0800

    update terraform template to include section headings

--- runway/templates/terraform/main.tf
+++ runway/templates/terraform/main.tf
@@ -1,15 +1,17 @@
+# Backend setup
 terraform {
   backend "s3" {
     key = "sampleapp.tfstate"
   }
 }
 
+# Variable definitions
 variable "region" {}
 
-# Specify the provider and access details
+# Provider and access setup
 provider "aws" {
   version = "~> 0.1"
   region = "${var.region}"
 }
 
-# Continue with data and resources here
+# Data and resources
