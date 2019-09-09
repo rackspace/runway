ffc12f3b3a99ffe2ea850f942f24efd3de09f476
Kyle Finley <kyle@finley.sh>
2019-09-09 09:37:09 -0700

    address issue with not being able to use when installed globally

--- npm/preuninstall.js
+++ npm/preuninstall.js
@@ -3,14 +3,24 @@ const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
-var runwayExe = 'runway';
+const basepath = `${path.resolve(process.cwd(), '../..')}/node_modules`; // goes to the top level node_modules
 
 if (os.platform() === 'win32') {
-  runwayExe = 'runway.bat';
+  if (process.env.npm_config_global) {
+    binPath = path.resolve(process.env.APPDATA, './npm/runway.bat');
+  } else {
+    binPath = `${basepath}/.bin/runway.bat`;
+  }
+} else {
+  if (process.env.npm_config_global) {
+    binPath = '/usr/local/bin/runway';
+  } else {
+    binPath = `${basepath}/.bin/runway`;
+  }
 }
 
 // remove symlink/exe from bin created by postinstall script
-fs.unlink(path.resolve(__dirname, `../.bin/${runwayExe}`), (err, data) => {
+fs.unlink(binPath, (err, data) => {
   if (err) {
     // ignore file/dir missing
     if (err.code !== 'ENOENT') {
