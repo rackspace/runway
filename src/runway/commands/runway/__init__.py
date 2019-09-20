3a837c22c6ef4e98eb2eed52cfe728d3d49a6dc4
Kyle Finley <kyle@finley.sh>
2019-09-20 08:46:25 -0700

    revert remove run_python
    - include SKIP_FIND_CONFIG

--- src/runway/commands/runway/__init__.py
+++ src/runway/commands/runway/__init__.py
@@ -6,6 +6,7 @@ from .gen_sample import *  # noqa
 from .gitclean import *  # noqa
 from .init import *  # noqa
 from .preflight import *  # noqa
+from .run_python import *  # noqa
 from .run_stacker import *  # noqa
 from .test import *  # noqa
 from .whichenv import *  # noqa
---
+++ src/runway/commands/runway/run_python.py
@@ -0,0 +1,21 @@
+"""The run-python command."""
+
+import logging
+import sys
+
+from ..runway_command import RunwayCommand
+from ...util import get_embedded_lib_path
+
+LOGGER = logging.getLogger('runway')
+
+
+class RunPython(RunwayCommand):
+    """Extend RunwayCommand with execution of python files."""
+
+    SKIP_FIND_CONFIG = True
+
+    def execute(self):
+        """Execute python script."""
+        sys.path.insert(1, get_embedded_lib_path())
+        with open(self._cli_arguments.get('<filename>'), 'r') as stream:
+            exec(stream.read())  # pylint: disable=exec-used
