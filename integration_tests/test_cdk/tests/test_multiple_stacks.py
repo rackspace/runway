8d27977eba1d7f464243440e01d34d3a7dc76134
Shane Smith <voodoogq@gmail.com>
2019-12-10 09:31:12 -0600

    Working through pexpect scenarios on launch

--- integration_tests/test_cdk/tests/test_multiple_stacks.py
+++ integration_tests/test_cdk/tests/test_multiple_stacks.py
@@ -1,7 +1,11 @@
 """Test that multiple CDK stacks does not prompt a failure"""
+import sys
+import pexpect
+
 from test_cdk.test_cdk import TestCDK
 from runway.util import change_dir
 from util import run_command
+
 
 class TestMultipleStacks(TestCDK):
     """Test deploying multiple stacks and ensure all are deployed"""
@@ -17,12 +21,21 @@ class TestMultipleStacks(TestCDK):
         self.copy_runway('multiple-stacks')
         self.copy_fixture()
         with change_dir(self.cdk_test_dir):
-            return run_command(['runway', 'deploy'])
+            child = pexpect.spawn('runway deploy', encoding='utf-8')
+            child.logfile = sys.stdout
+            child.expect("Do you wish to deploy these changes (y/n)?", timeout=120)
+            child.sendline('y')
+            child.expect("Do you wish to deploy these changes (y/n)?", timeout=120)
+            child.sendline('y')
+            child.expect('Stack ARN:')
+            # return run_command(['runway', 'deploy'])
+            # self.logger.info('foo')
 
     def run(self):
         """Run tests."""
         self.clean()
-        assert self.deploy() == 0, '{}: Multiple Stacks failed'.format(__name__)
+        self.deploy()
+        # assert self.deploy() == 0, '{}: Multiple Stacks failed'.format(__name__)
 
     def teardown(self):
         self.logger.info('Tearing down: %s', self.TEST_NAME)
