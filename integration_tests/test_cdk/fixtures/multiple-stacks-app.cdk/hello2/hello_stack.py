6087da0616724cd5fa9b9b31c684f07182d71083
Shane Smith <voodoogq@gmail.com>
2019-12-10 09:37:22 -0600

    Simplifying fixture build

--- integration_tests/test_cdk/fixtures/multiple-stacks-app.cdk/hello2/hello_stack.py
+++ integration_tests/test_cdk/fixtures/multiple-stacks-app.cdk/hello2/hello_stack.py
@@ -6,26 +6,12 @@ from aws_cdk import (
     core
 )
 
-from .hello_construct import Hello2Construct
-
-
 class MyStack2(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "MySecondQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
-
         topic = sns.Topic(
             self, "MySecondTopic",
             display_name="My Second Topic"
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
-
-        hello = Hello2Construct(self, "MyHelloConstruct", num_buckets=4)
-        user = iam.User(self, "MyUser")
-        hello.grant_read(user)
