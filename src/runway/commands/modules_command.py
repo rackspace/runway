6b5d96da0fc38ab2d362441fe38bfc617ae8ac66
Shane Smith <voodoogq@gmail.com>
2019-12-13 12:20:54 -0600

    Adding ref structure, downloading appropriate repo, properly identifying full folder path

--- src/runway/commands/modules_command.py
+++ src/runway/commands/modules_command.py
@@ -26,6 +26,7 @@ from ..util import (
     change_dir, load_object_from_string, merge_dicts,
     merge_nested_environment_dicts
 )
+import subprocess
 
 if sys.version_info[0] > 2:
     import concurrent.futures
@@ -345,11 +346,8 @@ class SourceProcessor(object):
     def get_config(self, path):
        conf = {}
        params = path.split('>')
+       conf = { key: value for key, value in [param.split('=') for param in params[1:]] }
        conf['uri'] = params[0]
-
-       for x in range(1, len(params)):
-           key, value = params[x].split('=')
-           conf[key] = value
 
        return conf
 
@@ -364,18 +362,66 @@ class SourceProcessor(object):
     def fetch_git_package(self):
         from git import Repo
 
-        dir_name = self.sanitize_git_path(self.config['uri'])
+        ref = self.determine_git_ref()
+        dir_name = '_'.join([self.sanitize_git_path(self.config['uri']), ref])
         cached_dir_path = os.path.join(self.cache_dir, dir_name)
 
         if not os.path.isdir(cached_dir_path):
             tmp_dir = tempfile.mkdtemp()
             try:
                 tmp_repo_path = os.path.join(tmp_dir, dir_name)
-                Repo.clone_from(self.config['uri'], tmp_repo_path)
+                with Repo.clone_from(self.config['uri'], tmp_repo_path) as repo:
+                    repo.head.reference = ref
+                    repo.head.reset(index=True, working_tree=True)
                 shutil.move(tmp_repo_path, self.cache_dir)
-                config['cached_path'] = os.path.join(self.cache_dir, dir_name)
+                self.config['cached_path'] = os.path.join(self.cache_dir, dir_name)
             finally:
                 shutil.rmtree(tmp_dir)
+        else:
+            self.config['cached_path'] = cached_dir_path
+
+        self.config['full_path'] = os.path.join(self.config['cached_path'], self.config['location'])
+
+    def git_ls_remote(self, ref):
+        LOGGER.debug("Invoking git to retrieve commit id for repo %s...", self.config['uri'])
+        lsremote_output = subprocess.check_output(['git',
+                                                   'ls-remote',
+                                                   self.config['uri'],
+                                                   ref])
+        if b"\t" in lsremote_output:
+            commit_id = lsremote_output.split(b"\t")[0]
+            LOGGER.debug("Matching commit id found: %s", commit_id)
+            return commit_id
+        else:
+            raise ValueError("Ref \"%s\" not found for repo %s." % (ref, self.config['uri']))
+
+    def determine_git_ls_remote_ref(self):
+        if self.config.get('branch'):
+            ref = "refs/heads/%s" % self.config['branch']
+        else:
+            ref = "HEAD"
+
+        return ref
+
+    def determine_git_ref(self):
+        ref_config_keys = 0
+        for i in ['commit', 'tag', 'branch']:
+            if self.config.get(i):
+                ref_config_keys += 1
+        if ref_config_keys > 1:
+            raise ImportError("Fetching remote git sources failed: "
+                              "conflicting revisions (e.g. 'commit', 'tag', "
+                              "'branch') specified for a package source")
+
+        if self.config.get('commit'):
+            ref = self.config['commit']
+        elif self.config.get('tag'):
+            ref = self.config['tag']
+        else:
+            ref = self.git_ls_remote(self.determine_git_ls_remote_ref())
+        if sys.version_info[0] > 2 and isinstance(ref, bytes):
+            return ref.decode()
+        return ref
 
     def sanitize_git_path(self, path):
         dir_name = path
@@ -602,6 +648,7 @@ class ModulesCommand(RunwayCommand):
                 module['path'],
                 os.path.join(self.env_root, '.runway_cache')
             )
+            module_root = processor.config.get('full_path')
         else:
             module_root = os.path.join(self.env_root, module['path'])
         module_opts = merge_dicts(module_opts, module.__dict__)
