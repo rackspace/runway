585ae4814bb441ff4239d8bee19c7a826c23d671
Shane Smith <voodoogq@gmail.com>
2019-12-16 09:07:46 -0600

    Updating variable name to 'location' from path (no Path.path)

--- src/runway/path.py
+++ src/runway/path.py
@@ -1,28 +1,30 @@
-from .sources.git import Git
-import tempfile
-import shutil
-import subprocess
-
+""" Runway configuration 'path' settings """
 import os
-import sys
-import six
 import logging
+import six
+
+from .sources.git import Git
 
 LOGGER = logging.getLogger('runway')
 
 class Path(object):
+    """ Runway configuration 'path' settings object """
+
     def __init__(self, module, env_root, cache_dir=None):
+        """ Initialize. """
         if not cache_dir:
             cache_dir = os.path.expanduser("~/.runway_cache")
 
         self.module = module
         self.env_root = env_root
         self.cache_dir = cache_dir
-        self.source, self.path = self.get_source_and_path()
+        self.source, self.location = self.get_source_and_location()
         self.config = self.get_config()
         self.module_root = self.get_module_root()
 
-    def get_source_and_path(self):
+    def get_source_and_location(self):
+        """ Retrieve the source and location of the path variable """
+
         res = ['local', '']
         sp = self.module['path'].split('::')
 
@@ -37,20 +39,20 @@ class Path(object):
         if isinstance(self.module, six.string_types):
             self.module = {'path': self.module}
 
-        if self.path in ['.', '.' + os.sep]:
+        if self.location in ['.', '.' + os.sep]:
             return self.env_root
         if self.source != 'local':
             self.create_cache_directory()
             return self.fetch_remote_source()
-        return os.path.join(self.env_root, self.path)
+        return os.path.join(self.env_root, self.location)
 
     def get_config(self):
-       conf = {}
-       params = self.path.split('>')
-       conf = { key: value for key, value in [param.split('=') for param in params[1:]] }
-       conf['uri'] = params[0]
+        conf = {}
+        params = self.location.split('>')
+        conf = { key: value for key, value in [param.split('=') for param in params[1:]] }
+        conf['uri'] = params[0]
 
-       return conf
+        return conf
 
     def create_cache_directory(self):
         if not os.path.isdir(self.cache_dir):
