1d915c21bc37b39491d36e65113e05d5de1e2d70
Shane Smith <voodoogq@gmail.com>
2019-12-16 15:47:06 -0600

    Typing

--- src/runway/path.py
+++ src/runway/path.py
@@ -1,4 +1,7 @@
 """Runway configuration 'path' settings."""
+# pylint: disable=unused-import
+from typing import Optional, Dict, List, Tuple  # noqa: F401
+
 import os
 import logging
 import six
@@ -12,19 +15,24 @@ class Path(object):
     """Runway configuration 'path' settings object."""
 
     def __init__(self, module, env_root, cache_dir=None):
+        # type: (str, str, Optional[str])-> Path
         """Initialize."""
         if not cache_dir:
-            cache_dir = os.path.expanduser("~/.runway_cache")
+            cache_dir = os.path.expanduser("~/.runway_cache")  # type: str
 
-        self.env_root = env_root
-        self.cache_dir = cache_dir
-        self.source, self.uri, self.location, self.options = self.parse(module)
-        self.module_root = self.__get_module_root_dir(module)
+        self.env_root = env_root  # type: str
+        self.cache_dir = cache_dir  # type: str
+        (self.source,
+         self.uri,
+         self.location,
+         self.options) = self.parse(module)  # type: Tuple[str]
+        self.module_root = self.__get_module_root_dir(module)  # type: str
 
     def __get_module_root_dir(self, module):
+        # type: (str) -> str
         """Retrieve the root directory location of the module being parsed."""
         if isinstance(module, six.string_types):
-            module = {'path': module}
+            module = {'path': module}  # type: Dict[str, str]
 
         if self.location in ['.', '.' + os.sep]:
             return self.env_root
@@ -34,11 +42,13 @@ class Path(object):
         return os.path.join(self.env_root, self.location)
 
     def __create_cache_directory(self):
+        # type: () -> None
         """If no cache directory exists for the remote runway modules, create one."""
         if not os.path.isdir(self.cache_dir):
             os.mkdir(self.cache_dir)
 
     def __fetch_remote_source(self):
+        # type: () -> Git or None
         """
         Switch based on the retrieved source of the path.
 
@@ -50,46 +60,48 @@ class Path(object):
 
     @property
     def configuration(self):
+        # type: () -> Dict[str, str]
         """Transform object into configuration settings for remote sources."""
-        conf = {
+        return {
             'source': self.source,
             'location': self.location,
             'uri': self.uri,
             'options': self.options
         }
-        return conf
 
     @classmethod
     def parse(cls, module):
+        # type: (str) -> Tuple[str]
         """Retrieve the source and location of the path variable."""
-        source = 'local'
-        uri = ''
-        location = ''
-        options = ''
+        source = 'local'  # type: str
+        uri = ''  # type: str
+        location = ''  # type: str
+        options = ''  # type: str
 
-        split_source_location = module.get('path', '').split('::')
+        split_source_location = module.get('path', '').split('::')  # type: List[str]
 
         # Local path
         if len(split_source_location) != 2:
-            location = split_source_location[0]
-            return [source, uri, location, options]
+            location = split_source_location[0]  # type: str
+            return source, uri, location, options
 
-        source = split_source_location[0]
-        temp_location = split_source_location[1]
+        source = split_source_location[0]  # type: str
+        temp_location = split_source_location[1]  # type: str
 
-        uri, location = cls.__parse_uri_and_location(temp_location)
-        location, options = cls.__parse_location_and_options(location)
+        uri, location = cls.__parse_uri_and_location(temp_location)  # type: List[str]
+        location, options = cls.__parse_location_and_options(location)  # type: List[str]
 
         return source, uri, location, options
 
     @classmethod
     def __parse_uri_and_location(cls, uri_loc_str):
+        # type: (str) -> List[str]
         """Given a location string extract the uri and remaining location values."""
-        split_uri_location = uri_loc_str.split('//')
-        location_string = '/'
+        split_uri_location = uri_loc_str.split('//')  # type: List[str, str]
+        location_string = '/'  # type: str
 
         if len(split_uri_location) == 3:
-            location_string = split_uri_location[2]
+            location_string = split_uri_location[2]  # type: str
 
         return [
             '//'.join([split_uri_location[0], split_uri_location[1]]),
@@ -98,24 +110,28 @@ class Path(object):
 
     @classmethod
     def __parse_location_and_options(cls, loc_opt_str):
+        # type: (str) -> List[str]
         """Given a location string extract the location variable and the remote module options."""
-        split_location_options = loc_opt_str.split('?')
-        location = split_location_options[0]
-        options = {}
+        split_location_options = loc_opt_str.split('?')  # type: List(str)
+        location = split_location_options[0]  # type: str
+        options = {}  # type: Dict
 
         if len(split_location_options) == 2:
-            options = cls.__parse_options_dict(split_location_options[1])
+            options = cls.__parse_options_dict(
+                split_location_options[1]
+            )  # type: Dict[str, str]
 
         return [location, options]
 
     @staticmethod
     def __parse_options_dict(options_str):
+        # type: (str) -> Dict[str, str]
         """Convert the options string into a dict."""
-        opts = options_str.split('&')
-        res = {}
+        opts = options_str.split('&')  # type: List[str]
+        res = {}  # Type: Dict
 
         for opt in opts:
-            key, value = opt.split('=')
-            res[key] = value
+            key, value = opt.split('=')  # type: List[str, str]
+            res[key] = value  # type: str
 
         return res
--- src/runway/sources/source.py
+++ src/runway/sources/source.py
@@ -4,6 +4,8 @@ Abstract parent class for a 'Source' type object.
 Allows us to specify specific remote sourced resources for out application
 (Git, S3, ect.)
 """
+# pylint: disable=unused-import
+from typing import Dict, Optional, Union  # noqa: F401
 
 
 class Source(object):
@@ -15,17 +17,20 @@ class Source(object):
     """
 
     def __init__(self, config, cache_dir=None):
+        # type(Dict[str, Union[str, Dict[str, str]]], Optional[str]) -> Source
         """Initialize."""
-        self.config = config
-        self.cache_dir = cache_dir
+        self.config = config  # type: Dict[str, Union[str, Dict[str, str]]]
+        self.cache_dir = cache_dir  # type: str
 
     def fetch(self):
+        # type: () -> None
         """Retrieve remote source. To be implemented in each subclass."""
         raise NotImplementedError
 
     @staticmethod
     def sanitize_directory_path(uri):
+        # type: (str) -> str
         """Sanitize a Source directory path string."""
         for i in ['@', '/', ':']:
-            uri = uri.replace(i, '_')
+            uri = uri.replace(i, '_')  # type: str
         return uri
