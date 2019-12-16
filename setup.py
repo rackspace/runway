4b8f2ffbfab970ddcdbef2fe56ca55ef487c8d9e
Shane Smith <voodoogq@gmail.com>
2019-12-16 15:20:38 -0600

    Adding GCG to setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ INSTALL_REQUIRES = [
     'docopt',
     'requests',
     'future',
+    'gcg',
     'gitpython',
     # embedded pyhcl is 0.3.12
     # with the LICENSE file added to its root folder
