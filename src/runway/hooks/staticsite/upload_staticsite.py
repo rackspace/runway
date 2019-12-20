367c66a9c4e716d487a8105bdb39dba8da942c80
Shane Smith <voodoogq@gmail.com>
2019-12-20 08:48:15 -0600

    General cleanup

--- src/runway/hooks/staticsite/upload_staticsite.py
+++ src/runway/hooks/staticsite/upload_staticsite.py
@@ -15,16 +15,18 @@ LOGGER = logging.getLogger(__name__)
 def get_archives_to_prune(archives, hook_data):
     """Return list of keys to delete."""
     files_to_skip = []
+
     for i in ['current_archive_filename', 'old_archive_filename']:
         if hook_data.get(i):
             files_to_skip.append(hook_data[i])
-    archives.sort(key=itemgetter('LastModified'),
-                  reverse=False)  # sort from oldest to newest
+
+    archives.sort(key=itemgetter('LastModified'), reverse=False)  # sort from oldest to newest
+
     # Drop all but last 15 files
     return [i['Key'] for i in archives[:-15] if i['Key'] not in files_to_skip]
 
 
-def sync(context, provider, **kwargs):  # pylint: disable=too-many-locals
+def sync(context, provider, **kwargs):
     """Sync static website to S3 bucket."""
     session = get_session(provider.region)
     bucket_name = OutputLookup.handle(kwargs.get('bucket_output_lookup'),
@@ -35,18 +37,6 @@ def sync(context, provider, **kwargs):  # pylint: disable=too-many-locals
         LOGGER.info('staticsite: skipping upload; latest version already '
                     'deployed')
     else:
-        distribution_id = OutputLookup.handle(
-            kwargs.get('distributionid_output_lookup'),
-            provider=provider,
-            context=context
-        )
-        distribution_domain = OutputLookup.handle(
-            kwargs.get('distributiondomain_output_lookup'),
-            provider=provider,
-            context=context
-        )
-        distribution_path = kwargs.get('distribution_path', '/*')
-
         # Using the awscli for s3 syncing is incredibly suboptimal, but on
         # balance it's probably the most stable/efficient option for syncing
         # the files until https://github.com/boto/boto3/issues/358 is resolved
@@ -56,33 +46,67 @@ def sync(context, provider, **kwargs):  # pylint: disable=too-many-locals
                  "s3://%s/" % bucket_name,
                  '--delete'])
 
-        cf_client = session.client('cloudfront')
-        cf_client.create_invalidation(
-            DistributionId=distribution_id,
-            InvalidationBatch={
-                'Paths': {
-                    'Quantity': 1,
-                    'Items': [distribution_path]},
-                'CallerReference': str(time.time())}
-        )
-        LOGGER.info("staticsite: sync & CF invalidation of %s (domain %s) "
-                    "complete",
-                    distribution_id,
-                    distribution_domain)
+        distribution = get_distribution_data(context=context, provider=provider, **kwargs)
+        invalidate_distribution(session=session, **distribution)
 
-        if not context.hook_data['staticsite'].get('hash_tracking_disabled'):
-            LOGGER.info("staticsite: updating environment SSM parameter %s "
-                        "with hash %s",
-                        context.hook_data['staticsite']['hash_tracking_parameter'],  # noqa
-                        context.hook_data['staticsite']['hash'])
-            ssm_client = session.client('ssm')
-            ssm_client.put_parameter(
-                Name=context.hook_data['staticsite']['hash_tracking_parameter'],  # noqa
-                Description='Hash of currently deployed static website source',
-                Value=context.hook_data['staticsite']['hash'],
-                Type='String',
-                Overwrite=True
-            )
+        LOGGER.info("staticsite: sync " "complete")
+
+        update_ssm_hash(context=context, session=session)
+
+    prune_archives(
+        context=context,
+        session=session
+    )
+    return True
+
+def update_ssm_hash(context=None, session=None):
+    if not context.hook_data['staticsite'].get('hash_tracking_disabled'):
+        LOGGER.info("staticsite: updating environment SSM parameter %s "
+                    "with hash %s",
+                    context.hook_data['staticsite']['hash_tracking_parameter'],  # noqa
+                    context.hook_data['staticsite']['hash'])
+        ssm_client = session.client('ssm')
+        ssm_client.put_parameter(
+            Name=context.hook_data['staticsite']['hash_tracking_parameter'],  # noqa
+            Description='Hash of currently deployed static website source',
+            Value=context.hook_data['staticsite']['hash'],
+            Type='String',
+            Overwrite=True
+        )
+    return True
+
+def get_distribution_data(context=None, provider=None, **kwargs):
+    LOGGER.info("Retrieved distribution data")
+    return {
+        'identifier': OutputLookup.handle(
+            kwargs.get('distributionid_output_lookup'),
+            provider=provider,
+            context=context
+        ),
+        'domain': OutputLookup.handle(
+            kwargs.get('distributiondomain_output_lookup'),
+            provider=provider,
+            context=context
+        ),
+        'path': kwargs.get('distribution_path', '/*')
+    }
+
+def invalidate_distribution(session=None, identifier='', path='', domain='', **_):
+    LOGGER.info("staticsite: Invalidating CF distribution")
+    cf_client = session.client('cloudfront')
+    cf_client.create_invalidation(
+        DistributionId=identifier,
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': 1,
+                'Items': [path]},
+            'CallerReference': str(time.time())}
+    )
+
+    LOGGER.info("staticsite: CF invalidation of %s (domain %s) " "complete", identifier, domain)
+    return True
+
+def prune_archives(context=None, session=None):
     LOGGER.info("staticsite: cleaning up old site archives...")
     archives = []
     s3_client = session.client('s3')
@@ -91,12 +115,14 @@ def sync(context, provider, **kwargs):  # pylint: disable=too-many-locals
         Bucket=context.hook_data['staticsite']['artifact_bucket_name'],
         Prefix=context.hook_data['staticsite']['artifact_key_prefix']
     )
+
     for page in response_iterator:
         archives.extend(page.get('Contents', []))
     archives_to_prune = get_archives_to_prune(
         archives,
         context.hook_data['staticsite']
     )
+
     # Iterate in chunks of 1000 to match delete_objects limit
     for objects in [archives_to_prune[i:i + 1000]
                     for i in range(0, len(archives_to_prune), 1000)]:
