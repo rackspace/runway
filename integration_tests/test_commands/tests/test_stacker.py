c96c186cb09c06fd0775ebf9f616252561fa462a
edgar poce <eponce@onica.com>
2020-01-07 15:49:16 -0300

    stacker deploy stack

--- integration_tests/test_commands/tests/test_stacker.py
+++ integration_tests/test_commands/tests/test_stacker.py
@@ -1,8 +1,13 @@
 """Test deploying stacker."""
 import os
 from subprocess import check_output
+import boto3
 
 from integration_tests.test_commands.test_commands import Commands
+
+CLIENT = boto3.client('ssm')
+KEY = "/runway/integration-test/stacker"
+VALUE = "foo"
 
 
 class TestRunStacker(Commands):
@@ -10,19 +15,38 @@ class TestRunStacker(Commands):
 
     TEST_NAME = __name__
 
+    def get_stack_path(self):
+        """Gets the stacker itest path."""
+        return os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'fixtures',
+            'stacker')
+
     def init(self):
         """Initialize test."""
         pass  # pylint: disable=unnecessary-pass
 
     def run(self):
         """Run tests."""
-        response = check_output(
+        path = self.get_stack_path()
+        check_output(
             ['runway',
              'run-stacker',
              '--',
-             '--version']).decode().strip()
-        assert response == 'stacker 1.7.0'
+             'build',
+             'stack.yaml'],
+            cwd=path).decode()
+        parameter = CLIENT.get_parameter(Name=KEY)
+        assert parameter['Parameter']['Value'] == VALUE
 
     def teardown(self):
         """Teardown any created resources."""
-        pass  # pylint: disable=unnecessary-pass
+        path = self.get_stack_path()
+        check_output(
+            ['runway',
+             'run-stacker',
+             '--',
+             'destroy',
+             'stack.yaml',
+             '--force'],
+            cwd=path).decode()
