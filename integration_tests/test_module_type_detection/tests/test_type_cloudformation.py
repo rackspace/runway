54283f0e4296c1d273fe08ec45ba9e318a51ead3
Shane Smith <voodoogq@gmail.com>
2020-01-16 12:54:03 -0600

    Tests for cloudformation/serverless/terraform explicit type

--- integration_tests/test_module_type_detection/tests/test_type_cloudformation.py
+++ integration_tests/test_module_type_detection/tests/test_type_cloudformation.py
@@ -8,23 +8,25 @@ from integration_tests.test_module_type_detection.test_module_type_detection imp
 )
 
 
-class TestTypeCDK(ModuleTypeDetection):
-    """Test to verify a 'type' definition of 'cdk' is respected."""
+class TestTypeCloudformation(ModuleTypeDetection):
+    """Test to verify a 'type' definition of 'cloudformation' is respected."""
 
     TEST_NAME = __name__
 
     def deploy(self):
         """Deploy provider."""
-        self.copy_fixture('type_cdk')
-        self.copy_runway('type-cdk')
+        self.copy_fixture('type_cloudformation')
+        self.copy_runway('type-cloudformation')
         with change_dir(self.mtd_test_dir):
             out = subprocess.check_output(['runway', 'deploy'], stderr=subprocess.STDOUT)
-            return 0 if "Skipping cdk deploy of type_cdk" in out.decode() else -1
+            return 0 if "Skipping stacker build" in out.decode() else -1
 
     def run(self):
         """Run tests."""
         self.clean()
-        assert self.deploy() == 0, '{}:Explicit Type Declaration of CDK Failed'.format(__name__)
+        assert self.deploy() == 0, '{}: Type Declaration of Cloudformation Failed'.format(
+            __name__
+        )
 
     def teardown(self):
         """Teardown."""
--- integration_tests/test_module_type_detection/tests/test_type_serverless.py
+++ integration_tests/test_module_type_detection/tests/test_type_serverless.py
@@ -8,23 +8,25 @@ from integration_tests.test_module_type_detection.test_module_type_detection imp
 )
 
 
-class TestTypeCDK(ModuleTypeDetection):
-    """Test to verify a 'type' definition of 'cdk' is respected."""
+class TestTypeServerless(ModuleTypeDetection):
+    """Test to verify a 'type' definition of 'serverless' is respected."""
 
     TEST_NAME = __name__
 
     def deploy(self):
         """Deploy provider."""
-        self.copy_fixture('type_cdk')
-        self.copy_runway('type-cdk')
+        self.copy_fixture('type_serverless')
+        self.copy_runway('type-serverless')
         with change_dir(self.mtd_test_dir):
             out = subprocess.check_output(['runway', 'deploy'], stderr=subprocess.STDOUT)
-            return 0 if "Skipping cdk deploy of type_cdk" in out.decode() else -1
+            return 0 if "Skipping serverless deploy of type_serverless" in out.decode() else -1
 
     def run(self):
         """Run tests."""
         self.clean()
-        assert self.deploy() == 0, '{}:Explicit Type Declaration of CDK Failed'.format(__name__)
+        assert self.deploy() == 0, '{}: Type Declaration of Serverless Failed'.format(
+            __name__
+        )
 
     def teardown(self):
         """Teardown."""
