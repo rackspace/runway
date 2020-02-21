cb4a31d6aa52ff4ee31f89127f64f901d9234cac
Kyle Finley <kyle@finley.sh>
2020-02-21 13:48:18 -0800

    add shim for stacker.variables

--- runway/__init__.py
+++ runway/__init__.py
@@ -1,7 +1,9 @@
 """Set package version."""
 import sys
 from . import cfngin
+from . import variables
 
 sys.modules['stacker'] = cfngin  # shim to remove stacker dependency
+sys.modules['stacker.variables'] = variables  # shim to support standard variables
 
 __version__ = '1.4.1'
