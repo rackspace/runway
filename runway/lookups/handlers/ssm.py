c64cfad71aa3af915478a243a639476ba7e94884
Kyle Finley <kyle@finley.sh>
2020-03-05 14:54:57 -0800

    add support for a default value

--- runway/lookups/handlers/ssm.py
+++ runway/lookups/handlers/ssm.py
@@ -1,11 +1,10 @@
 """Retrieve a value from SSM Parameter Store."""
 # pylint: disable=arguments-differ
 import logging
-from typing import Any, Union, TYPE_CHECKING  # pylint: disable=unused-import
+from typing import TYPE_CHECKING, Any, Union  # pylint: disable=unused-import
 
 # using absolute for runway imports so stacker shim doesn't break when used from CFNgin
 from runway.lookups.handlers.base import LookupHandler
-# from runway.util import MutableMap
 
 # python2 supported pylint sees this is cyclic even though its only for type checking
 # pylint: disable=cyclic-import
@@ -28,16 +27,17 @@ class SsmLookup(LookupHandler):
 
         session = context.get_session(region=args.get('region'))
         client = session.client('ssm')
-        response = client.get_parameters(
-            Names=[query],
-            WithDecryption=True
-        ).get('Parameters', [])[0]
 
-        if not response:
-            raise ValueError(
-                'SSM Parameter "{}" does not exist in region {}'.format(
-                    value, session.region_name))
-
-        return cls.format_results(response['Value'].split(',')
-                                  if response['Type'] == 'StringList'
-                                  else response['Value'], **args)
+        try:
+            response = client.get_parameter(
+                Name=query,
+                WithDecryption=True
+            )['Parameter']
+            return cls.format_results(response['Value'].split(',')
+                                      if response['Type'] == 'StringList'
+                                      else response['Value'], **args)
+        except client.exceptions.ParameterNotFound:
+            if args.get('default'):
+                args.pop('load', None)  # don't load a default value
+                return cls.format_results(args.pop('default'), **args)
+            raise
