1e7a071ce846ef586b26af34f5cb6a43498f119a
Shane Smith <voodoogq@gmail.com>
2020-03-16 12:39:35 -0500

    Fixing docstring call

--- runway/blueprints/staticsite/staticsite.py
+++ runway/blueprints/staticsite/staticsite.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python """Module with static website bucket and CloudFront distribution."""
+#!/usr/bin/env python
+"""Module with static website bucket and CloudFront distribution."""
 from __future__ import print_function
 
 import hashlib
