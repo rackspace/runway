ff47a68d37ec06d461cc4eaed52595256cbee955
Kyle Finley <kyle@finley.sh>
2020-03-26 12:36:23 -0700

    add support for setting ttl, refactor route 53 interactions

--- runway/cfngin/hooks/acm.py
+++ runway/cfngin/hooks/acm.py
@@ -3,8 +3,10 @@ import logging
 from time import sleep
 
 from botocore.exceptions import ClientError
+from troposphere import Ref
 from troposphere.certificatemanager import Certificate as CertificateResource
 
+from runway.cfngin.blueprints.variables.types import CFNString
 from runway.util import MutableMap
 
 from ..status import NO_CHANGE
@@ -32,6 +34,7 @@ class Certificate(Hook):
         hosted_zone_id (str): The ID of the Route 53 Hosted Zone that contains
             the resource record sets that you want to change. This must exist
             in the same account that the certificate will be created in.
+        ttl (int): The resource record cache time to live (TTL), in seconds.
 
     Example:
     .. code-block: yaml
@@ -56,6 +59,7 @@ class Certificate(Hook):
                 Provider instance. (passed in by CFNgin)
 
         """
+        kwargs.setdefault('ttl', 30)
         super(Certificate, self).__init__(context, provider, **kwargs)
 
         self.template_description = self.get_template_description()
@@ -72,7 +76,9 @@ class Certificate(Hook):
         session = self.context.get_session()
         self.acm_client = session.client('acm')
         self.r53_client = session.client('route53')
-        self.stack = self.generate_stack()
+        self.stack = self.generate_stack(
+            variables={'ValidateRecordTTL': self.args.ttl}
+        )
 
     def _create_blueprint(self):
         """Create CFNgin Blueprint."""
@@ -80,44 +86,22 @@ class Certificate(Hook):
         blueprint.template.set_version('2010-09-09')
         blueprint.template.set_description(self.template_description)
 
+        blueprint.VARIABLES = {
+            'ValidateRecordTTL': {
+                'type': CFNString,
+                'description': 'NO NOT CHANGE MANUALLY! Used to track the '
+                               'state of a value set outside of CloudFormation'
+                               ' using a Runway hook.'
+            }
+        }
+
         cert = blueprint.template.add_resource(CertificateResource(
             'Certificate',
             **self.properties.data
         ))
         blueprint.add_output('%sArn' % cert.title, cert.ref())
+        blueprint.add_output('ValidateRecordTTL', Ref('ValidateRecordTTL'))
         return blueprint
-
-    def remove_validation_records(self):
-        """Remove all record set entries used to validate an ACM Certificate."""
-        cert_arn = self.get_certificate()
-        cert = self.acm_client.describe_certificate(
-            CertificateArn=cert_arn
-        )['Certificate']
-
-        records = [opt['ResourceRecord']
-                   for opt in cert.get('DomainValidationOptions', [])
-                   if opt['ValidationMethod'] == 'DNS']
-        LOGGER.info('Removing %i validation record(s) from "%s"...',
-                    len(records), self.args.hosted_zone_id)
-        self.r53_client.change_resource_record_sets(
-            HostedZoneId=self.args.hosted_zone_id,
-            ChangeBatch={
-                'Comment': self.get_template_description(),
-                'Changes': [
-                    {
-                        'Action': 'DELETE',
-                        'ResourceRecordSet': {
-                            'Name': record['Name'],
-                            'Type': record['Type'],
-                            'ResourceRecords': [
-                                {'Value': record['Value']}
-                            ],
-                            'TTL': 5
-                        }
-                    } for record in records
-                ]
-            }
-        )
 
     def get_certificate(self, interval=5, stack_name=None):
         """Get the certificate being created by a CloudFormation.
@@ -144,7 +128,8 @@ class Certificate(Hook):
         sleep(interval)
         return self.get_certificate(stack_name)
 
-    def get_validation_record(self, cert_arn=None, interval=5, stack_name=None):
+    def get_validation_record(self, cert_arn=None, interval=5, stack_name=None,
+                              status='PENDING_VALIDATION'):
         """Get validation record from the certificate being created.
 
         Args:
@@ -152,6 +137,9 @@ class Certificate(Hook):
             interval (int): Number of seconds to wait between attempts.
             stack_name (str): Name of CloudFormation stack containing a pending
                 certificate.
+            status (str): Validation status to look for when finding a
+                validation record. Typically only "PENDING_VALIDATION" or
+                "SUCCESS" will be used.
 
         Returns:
             Dict[str, str]: A record set to be added to Route 53.
@@ -170,24 +158,27 @@ class Certificate(Hook):
         try:
             domain_validation = [
                 opt for opt in cert['DomainValidationOptions']
-                if opt['ValidationStatus'] == 'PENDING_VALIDATION'
+                if opt['ValidationStatus'] == status
             ]
         except KeyError:
             LOGGER.debug('Waiting for DomainValidationOptions to become '
                          'available for the certificate...')
             sleep(interval)
             return self.get_validation_record(cert_arn=cert_arn,
-                                              interval=interval)
+                                              interval=interval,
+                                              status=status)
 
         if not domain_validation:
-            raise ValueError('No pending validations found for "{}"'.format(
-                self.args.domain
-            ))
+            raise ValueError(
+                'No validations with status "{}" found for "{}"'.format(
+                    status, self.args.domain
+                )
+            )
         if len(domain_validation) > 1:
             raise ValueError(
-                'Found {} pending validation options for "{}"; only one '
+                'Found {} validation options of status "{}" for "{}"; only one '
                 'option is supported'.format(len(domain_validation),
-                                             self.args.domain)
+                                             status, self.args.domain)
             )
         try:
             # the validation option can exists before the record set is ready
@@ -197,7 +188,8 @@ class Certificate(Hook):
                          'to become available for the certificate...')
             sleep(interval)
             return self.get_validation_record(cert_arn=cert_arn,
-                                              interval=interval)
+                                              interval=interval,
+                                              status=status)
 
     def put_record_set(self, record_set):
         """Create/update a record set on a Route 53 Hosted Zone.
@@ -208,25 +200,63 @@ class Certificate(Hook):
         """
         LOGGER.info('Adding validation record to "%s"',
                     self.args.hosted_zone_id)
-        LOGGER.debug('Validation Record: %s', record_set)
-        return self.r53_client.change_resource_record_sets(
+        self.__change_record_set('ADD', [record_set])
+
+    def remove_validation_records(self):
+        """Remove all record set entries used to validate an ACM Certificate."""
+        cert_arn = self.get_certificate()
+        cert = self.acm_client.describe_certificate(
+            CertificateArn=cert_arn
+        )['Certificate']
+
+        records = [opt['ResourceRecord']
+                   for opt in cert.get('DomainValidationOptions', [])
+                   if opt['ValidationMethod'] == 'DNS']
+        LOGGER.info('Removing %i validation record(s) from "%s"...',
+                    len(records), self.args.hosted_zone_id)
+        self.__change_record_set('DELETE', records)
+
+    def update_record_set(self, record_set):
+        """Update a validation record set when the cert has not changed.
+
+        Args:
+            record_set (Dict[str, str]): Record set to be updated in Route 53.
+
+        """
+        LOGGER.info('Updating record set...')
+        self.__change_record_set('UPSERT', [record_set])
+
+    def __change_record_set(self, action, record_sets):
+        """Wrap boto3.client('acm').change_resource_record_sets.
+
+        Args:
+            action (str): Change action. [ADD, DELETE, UPSERT]
+            record_sets (List[Dict[str, str]]): Record sets to change.
+
+        """
+        if not record_sets:
+            raise ValueError('Must provide one of more record sets')
+
+        changes = [{
+            'Action': action,
+            'ResourceRecordSet': {
+                'Name': record['Name'],
+                'Type': record['Type'],
+                'ResourceRecords': [
+                    {'Value': record['Value']}
+                ],
+                'TTL': self.args.ttl
+            }
+        } for record in record_sets]
+
+        LOGGER.debug('Making the following changes to hosted zone "%s":\n%s',
+                     self.args.hosted_zone_id, changes)
+
+        self.r53_client.change_resource_record_sets(
             HostedZoneId=self.args.hosted_zone_id,
             ChangeBatch={
                 'Comment': self.get_template_description(),
-                'Changes': [
-                    {
-                        # 'Action': 'CREATE',  # TODO revert after adding delete
-                        'Action': 'UPSERT',  # create or update
-                        'ResourceRecordSet': {
-                            'Name': record_set['Name'],
-                            'Type': record_set['Type'],
-                            'ResourceRecords': [
-                                {'Value': record_set['Value']}
-                            ],
-                            'TTL': 5
-                        }
-                    }
-                ]
+                'Changes': changes
             }
         )
 
@@ -242,16 +272,16 @@ class Certificate(Hook):
 
         try:
             record = self.get_validation_record(cert_arn)
-            self.put_record_set(record)
-            LOGGER.info('Waiting for certificate to validate; '
-                        'this can take upwards of 30 minutes to complete...')
         except ValueError as err:
-            if 'No pending validations' in str(err) and \
+            if 'No validations with status "PENDING' in str(err) and \
                     'updating existing stack' in status.reason:
-                LOGGER.debug('Stack update did not cause recreation; '
-                             'no action required')
-            else:
-                raise
+                record = self.get_validation_record(cert_arn, status='SUCCESS')
+                self.update_record_set(record)
+                return result
+            raise
+        self.put_record_set(record)
+        LOGGER.info('Waiting for certificate to validate; '
+                    'this can take upwards of 30 minutes to complete...')
         self._wait_for_stack(self._deploy_action)
 
         return result
