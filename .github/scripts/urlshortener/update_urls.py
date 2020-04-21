f832aade8fe7dc0a177319e59f914ed624b5d09f
Kyle Finley <kyle@finley.sh>
2020-04-21 13:36:43 -0700

    add makefile for linting, add some unit tests

--- .github/scripts/urlshortener/update_urls.py
+++ .github/scripts/urlshortener/update_urls.py
@@ -1,7 +1,7 @@
 """Update Runway release URLs."""
 # pylint: disable=no-member
 import logging
-from typing import Optional, TypedDict, Union
+from typing import Optional, Union
 
 import boto3
 import click
@@ -20,11 +20,11 @@ TARGET_TEMPLATE = ('https://{bucket_name}.s3-{region}.amazonaws.com/runway/'
 OS_NAMES = ['linux', 'osx', 'windows']
 
 
-def get_version_from_tag_ref(_ctx: Optional[click.Context],
-                             _param: Optional[Union[click.Option, click.Parameter]],
-                             value: str
-                             ) -> str:
-    """Get package version from a GitHub Action's ref containing a tag.
+def sanitize_version(_ctx: Optional[click.Context],
+                     _param: Optional[Union[click.Option, click.Parameter]],
+                     value: str
+                     ) -> str:
+    """Sanitize a version number by stripping git tag ref and leading "v".
 
     To be used as the callback of a click option or parameter.
 
@@ -52,6 +52,41 @@ def put_item(table: Table, id_val: str, target: str) -> None:
                    ReturnValues='NONE')
 
 
+def handler(table: Table,
+            bucket_name: str,
+            bucket_region: str,
+            version: str,
+            latest: bool = False
+            ) -> None:
+    """Handle the command.
+
+    Core logic executed by the command aside from boto3 session/resource
+    initializeion and logging setup.
+
+    Args:
+        table: DynamoDB table resource.
+        bucket_name: Name of S3 Bucket where Runway artifact is located
+        bucket_region: AWS region where the S3 Bucket is located.
+        version: SemVer version being release.
+        latest: Update the "latest" URL.
+
+    """
+    for os_name in OS_NAMES:
+        target = TARGET_TEMPLATE.format(bucket_name=bucket_name,
+                                        os=os_name,
+                                        region=bucket_region,
+                                        version=version)
+        if os_name == 'windows':
+            target += '.exe'
+        if latest:
+            put_item(table=table,
+                     id_val=ID_TEMPLATE.format(release='latest', os=os_name),
+                     target=target)
+        put_item(table=table,
+                 id_val=ID_TEMPLATE.format(release=version, os=os_name),
+                 target=target)
+
+
 @click.command(context_settings={'help_option_names': ['-h', '--help'],
                                  'max_content_width': 999,
                                  'show_default': True})
@@ -64,7 +99,7 @@ def put_item(table: Table, id_val: str, target: str) -> None:
               help='Name of the DynamoDB table containing entries for the URL '
               'shortener.')
 @click.option('--version', metavar='<version>', required=True,
-              callback=get_version_from_tag_ref,
+              callback=sanitize_version,
               help='Runway version being release.')
 @click.option('--table-region', metavar='<table-region>', default='us-east-1',
               help='AWS region where the DynamoDB table is located.')
@@ -83,20 +118,7 @@ def command(bucket_name: str,
     session: Session = boto3.session.Session(region_name=table_region)
     table: Table = session.resource('dynamodb').Table(table_name)
 
-    for os_name in OS_NAMES:
-        target = TARGET_TEMPLATE.format(bucket_name=bucket_name,
-                                        os=os_name,
-                                        region=bucket_region,
-                                        version=version)
-        if os_name == 'windows':
-            target += '.exe'
-        if latest:
-            put_item(table=table,
-                     id_val=ID_TEMPLATE.format(release='latest', os=os_name),
-                     target=target)
-        put_item(table=table,
-                 id_val=ID_TEMPLATE.format(release=version, os=os_name),
-                 target=target)
+    handler(table, bucket_name, bucket_region, version, latest)
 
 
 if __name__ == '__main__':
