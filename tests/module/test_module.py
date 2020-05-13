d96fb6c51a62d932938b46b6a89314b64c26555a
Kyle Finley <kyle@finley.sh>
2020-05-13 15:48:06 -0700

    add ability to extend a serverless.yml from runway.yml (#268)
    * implement option parsing similar to terraform module
    
    * improve options handling, add run_cmd & print methods
    
    * add base class for npm modules, refactor sls module for easier testing
    
    sls module now has an extend_serverless_yml option that if provided will cause runway to create a temporary file containing the resolved contents of the serverless config and anything provided in the options
    
    split up the logic contained in the single run_serverless method of the class to make testing much easier. some of the logic/similar logic was moved into the base class for easier use across sls, cdk, and staticsite module types
    
    * add tests for RunwayModuleNpm
    
    * add tests for ServerlessOptions
    
    * add tests for Serverless, small refactoring for issues found in testing
    
    * fix python 2 linting
    
    * fix options NoneType
    
    * fix python 2 tests
    
    * update docs
    
    * add friendly exit when promotezip but not bucketname in Serverless obj
    
    * switch rel to abs b/c rel was not being found, copy list before extend
    
    * update changelog
    
    * improve test coverage

--- tests/module/test_module.py
+++ tests/module/test_module.py
@@ -1,17 +1,144 @@
 """Test runway.module.__init__."""
-# pylint: disable=no-self-use
+# pylint: disable=no-self-use,unused-argument
+import logging
 import sys
 from contextlib import contextmanager
 
 import pytest
+from mock import MagicMock, call, patch
 
-from runway.module import ModuleOptions
+from runway.module import NPM_BIN, ModuleOptions, RunwayModuleNpm
+
+if sys.version_info[0] > 2:  # TODO remove after droping python 2
+    from pathlib import Path  # pylint: disable=E
+else:
+    from pathlib2 import Path  # pylint: disable=E
 
 
 @contextmanager
 def does_not_raise():
     """Use for conditional pytest.raises when using parametrize."""
     yield
+
+
+class TestRunwayModuleNpm(object):
+    """Test runway.module.RunwayModuleNpm."""
+
+    @patch('runway.module.which')
+    @patch('runway.module.warn_on_boto_env_vars', MagicMock(return_value=None))
+    def test_check_for_npm(self, mock_which, caplog, runway_context):
+        """Test check_for_npm."""
+        mock_which.side_effect = ['something',  # first call during init
+                                  None]  # explicit call
+        obj = RunwayModuleNpm(context=runway_context,
+                              path='./tests',
+                              options={})
+        caplog.set_level(logging.WARNING, logger='runway')
+
+        with pytest.raises(SystemExit):
+            assert not obj.check_for_npm()
+
+        assert ['tests: "npm" not found in path or is not executable; '
+                'please ensure it is installed correctly.'] == \
+            caplog.messages
+
+    @patch.object(RunwayModuleNpm, 'check_for_npm', MagicMock(return_value=None))
+    @patch('runway.module.warn_on_boto_env_vars')
+    def test_init(self, mock_warn, runway_context, tmp_path):
+        """Test init and the attributes set in init."""
+        # pylint: disable=no-member
+        options = {
+            'environments': True,  # can only be True of a falsy value (but not dict)
+            'options': {'test_option': 'option_value'},
+            'parameters': {'test_parameter': 'parameter_value'},
+            'extra': 'something'
+        }
+        obj = RunwayModuleNpm(context=runway_context,
+                              path=str(tmp_path),
+                              options=options.copy())
+
+        obj.check_for_npm.assert_called_once()
+        assert obj.context == runway_context
+        assert obj.environments == options['environments']
+        assert obj.extra == options['extra']
+        assert obj.options == options['options']
+        assert obj.parameters == options['parameters']
+        assert isinstance(obj.path, Path)
+        assert str(obj.path) == str(tmp_path)
+        mock_warn.assert_called_once_with(runway_context.env_vars)
+
+        obj = RunwayModuleNpm(context=runway_context,
+                              path=tmp_path)
+        assert not obj.environments
+        assert not obj.options
+        assert not obj.parameters
+        assert obj.path == tmp_path
+
+    @patch('runway.module.format_npm_command_for_logging')
+    def test_log_npm_command(self, mock_log, caplog, patch_module_npm,
+                             runway_context):
+        """Test log_npm_command."""
+        mock_log.return_value = 'success'
+        obj = RunwayModuleNpm(context=runway_context,
+                              path='./tests',
+                              options={})
+        caplog.set_level(logging.INFO, logger='runway')
+        assert not obj.log_npm_command(['npm', 'test'])
+        mock_log.assert_called_once_with(['npm', 'test'])
+        assert ['tests: Running "success"'] == caplog.messages
+
+    @patch('runway.module.use_npm_ci')
+    @patch('runway.module.subprocess')
+    def test_npm_install(self, moc_proc, mock_ci, caplog, patch_module_npm,
+                         runway_context):
+        """Test npm_install."""
+        caplog.set_level(logging.INFO, logger='runway')
+        obj = RunwayModuleNpm(context=runway_context,
+                              path='./tests',
+                              options={})
+        expected_logs = []
+        expected_calls = []
+
+        obj.options['skip_npm_ci'] = True
+        assert not obj.npm_install()
+        expected_logs.append('tests: Skipping npm ci and npm install...')
+
+        obj.options['skip_npm_ci'] = False
+        obj.context.env_vars['CI'] = True
+        mock_ci.return_value = True
+        assert not obj.npm_install()
+        expected_logs.append('tests: Running npm ci...')
+        expected_calls.append(call([NPM_BIN, 'ci']))
+
+        obj.context.env_vars['CI'] = False
+        assert not obj.npm_install()
+        expected_logs.append('tests: Running npm install...')
+        expected_calls.append(call([NPM_BIN, 'install']))
+
+        obj.context.env_vars['CI'] = True
+        mock_ci.return_value = False
+        assert not obj.npm_install()
+        expected_logs.append('tests: Running npm install...')
+        expected_calls.append(call([NPM_BIN, 'install']))
+
+        assert expected_logs == caplog.messages
+        moc_proc.check_call.assert_has_calls(expected_calls)
+
+    def test_package_json_missing(self, caplog, patch_module_npm,
+                                  runway_context, tmp_path):
+        """Test package_json_missing."""
+        caplog.set_level(logging.WARNING, logger='runway')
+        obj = RunwayModuleNpm(context=runway_context,
+                              path=str(tmp_path),
+                              options={})
+
+        assert obj.package_json_missing()
+        assert [
+            '{}: Module is missing a "package.json"'.format(tmp_path.name)
+        ] == caplog.messages
+
+        (tmp_path / 'package.json').touch()
+        assert not obj.package_json_missing()
 
 
 class TestModuleOptions(object):
