253f497682ec7293f5164a5e6277200ad30196ff
Kyle Finley <kyle@finley.sh>
2020-06-11 14:40:37 -0700

    add run_stacker

--- runway/_cli/commands/__init__.py
+++ runway/_cli/commands/__init__.py
@@ -4,11 +4,13 @@ from .init import init
 from .kbenv import kbenv
 from .run_aws import run_aws
 from .run_python import run_python
+from .run_stacker import run_stacker
 
 __all__ = [
     'gen_sample',
     'init',
     'kbenv',
     'run_aws',
-    'run_python'
+    'run_python',
+    'run_stacker'
 ]
---
+++ runway/_cli/commands/run_stacker.py
@@ -0,0 +1,31 @@
+"""``runway run-stacker`` command."""
+import logging
+from typing import Tuple  # noqa pylint: disable=unused-import
+
+import click
+
+from ...cfngin.logger import setup_logging
+from ...cfngin.commands import Stacker
+from ...util import SafeHaven
+
+LOGGER = logging.getLogger(__name__)
+
+
+@click.command('run-stacker', short_help='deprecated, bundled stacker',
+               context_settings={'ignore_unknown_options': True})
+@click.argument('args', metavar='<args>', nargs=-1, required=True)
+def run_stacker(args):
+    # type: (Tuple[str, ...]) -> None
+    """Execute a command using the "shimmed" Stacker (aka CFNgin).
+
+    Depreacted since version 1.5.0.
+
+    """
+    LOGGER.warning('This command as been deprecated and will be removed in '
+                   'the next major release.')
+    arg_list = list(args)
+    with SafeHaven(argv=['stacker'] + arg_list):
+        stacker = Stacker(setup_logging=setup_logging)
+        cmd = stacker.parse_args(arg_list)
+        stacker.configure(cmd)
+        cmd.run(cmd)
