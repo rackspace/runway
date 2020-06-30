3c40c351c3f9bdccec8d97b224f71b9a3530eb58
Kyle Finley <kyle@finley.sh>
2020-06-30 09:20:59 -0700

    fix indent

--- runway/_cli/commands/_test.py
+++ runway/_cli/commands/_test.py
@@ -76,7 +76,7 @@ def test(ctx, **_):
             if not isinstance(err, SystemExit):
                 traceback.print_exc()
             elif err.code == 0:
-                    continue  # tests with zero exit code don't indicate failure
+                continue  # tests with zero exit code don't indicate failure
             LOGGER.error('Test failed: %s', tst.name)
             if tst.required:
                 LOGGER.error('Failed test was required, the remaining '
