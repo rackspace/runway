7c28f6ccfffb7b9e41a8a1f7b3d304a689757417
Kyle Finley <kyle@finley.sh>
2020-07-28 09:29:49 -0700

    update tfenv integration tests

--- runway/_cli/commands/_tfenv/_run.py
+++ runway/_cli/commands/_tfenv/_run.py
@@ -7,6 +7,7 @@ from typing import Any, Tuple  # noqa pylint: disable=W
 import click
 
 from ....env_mgr.tfenv import TFEnvManager
+from ....util import DOC_SITE
 from ... import options
 
 LOGGER = logging.getLogger(__name__.replace('._', '.'))
@@ -30,4 +31,22 @@ def run(ctx, args, **_):
     before the Terraform command.
 
     """
-    ctx.exit(subprocess.call([TFEnvManager().install()] + list(args)))
+    try:
+        ctx.exit(subprocess.call([TFEnvManager().install()] + list(args)))
+    except ValueError as err:
+        LOGGER.debug('terraform install failed', exc_info=True)
+        if 'unable to find' not in str(err):
+            LOGGER.error(
+                'unexpected error encountered when trying to install '
+                'Terraform',
+                exc_info=True
+            )
+            ctx.exit(1)
+        else:
+            LOGGER.error('unable to find a .terraform-version file')
+            LOGGER.error(
+                'learn how to use Runway to manage Terraform versions at '
+                '%s/page/terraform/advanced_features.html#version-management',
+                DOC_SITE
+            )
+        ctx.exit(1)
