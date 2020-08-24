ea8d24c6ab95496525ff126725d2de422e320dfa
Kyle Finley <kyle@finley.sh>
2020-08-24 13:54:38 -0700

    add classes to simplify working with an s3 bucket

--- runway/core/providers/aws/__init__.py
+++ runway/core/providers/aws/__init__.py
@@ -1,6 +1,7 @@
 """Runway AWS objects."""
+from . import s3
 from ._account import AccountDetails
 from ._assume_role import AssumeRole
 from ._cli import cli
 
-__all__ = ["AccountDetails", "AssumeRole", "cli"]
+__all__ = ["AccountDetails", "AssumeRole", "cli", "s3"]
---
+++ runway/core/providers/aws/s3/__init__.py
@@ -0,0 +1,4 @@
+"""AWS S3 objects."""
+from ._bucket import Bucket
+
+__all__ = ["Bucket"]
