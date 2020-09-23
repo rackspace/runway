c6228e968c440fde83e98b0b142ee6279bf846c2
Kyle Finley <kyle@finley.sh>
2020-09-23 09:11:54 -0700

    add tests for config model base and utils

---
+++ tests/unit/config/models/test_base.py
@@ -0,0 +1,88 @@
+"""Test runway.config.models.base."""
+# pylint: disable=no-self-use
+from typing import Any, Dict, Optional
+
+import pytest
+from pydantic import Extra, ValidationError
+
+from runway.config.models.base import ConfigProperty
+
+
+class BadObject(ConfigProperty):
+    """Subclass uses to test a parent class.
+
+    This class contains bad configuration.
+
+    """
+
+    name: str = ("invalid",)
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Model configuration."""
+
+        extra = Extra.forbid
+
+
+class GoodObject(ConfigProperty):
+    """Subclass used to test a parent class.
+
+    This class contains good configuration.
+
+    """
+
+    name: str
+    bool_field: bool = True
+    dict_field: Dict[str, Any] = {}
+    optional_str_field: Optional[str] = None
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Model configuration."""
+
+        extra = Extra.forbid
+
+
+class TestConfigProperty:
+    """Test runway.config.models.base.ConfigProperty."""
+
+    def test_contains(self) -> None:
+        """Test __contains__."""
+        obj = GoodObject(name="test")
+        assert "name" in obj
+        assert "missing" not in obj
+
+    def test_get(self) -> None:
+        """Test get."""
+        obj = GoodObject(name="test")
+        assert obj.get("name") == "test"
+        assert not obj.get("missing")
+        assert obj.get("missing", "default") == "default"
+
+    def test_getitem(self) -> None:
+        """Test __getitem__."""
+        assert GoodObject(name="test")["name"] == "test"
+
+    def test_setitem(self) -> None:
+        """Test __setitem__."""
+        obj = GoodObject(name="test")
+        assert obj.name == "test"
+        obj["name"] = "new"
+        assert obj.name == "new"
+
+    def test_validate_all(self) -> None:
+        """Test Config.validate_all."""
+        with pytest.raises(ValidationError) as excinfo:
+            BadObject()
+        errors = excinfo.value.errors()
+        assert len(errors) == 1
+        assert errors[0]["loc"] == ("name",)
+        assert errors[0]["msg"] == "str type expected"
+
+    def test_validate_assignment(self) -> None:
+        """Test Config.validate_assignment."""
+        with pytest.raises(ValidationError) as excinfo:
+            obj = GoodObject(name="test")
+            obj.name = ("invalid",)
+        errors = excinfo.value.errors()
+        assert len(errors) == 1
+        assert errors[0]["loc"] == ("name",)
+        assert errors[0]["msg"] == "str type expected"
---
+++ tests/unit/config/models/test_utils.py
@@ -0,0 +1,70 @@
+"""Test runway.config.models.utils."""
+# pylint: disable=no-self-use
+from pathlib import Path
+from typing import Any, Optional
+
+import pytest
+
+from runway.config.models.utils import (
+    RUNWAY_LOOKUP_STRING_ERROR,
+    convert_null_values,
+    resolve_path_field,
+    validate_string_is_lookup,
+)
+
+
+@pytest.mark.parametrize(
+    "provided, expected",
+    [
+        ("null", None),
+        ("none", None),
+        ("undefined", None),
+        ("None", None),
+        ("retain", "retain"),
+        ("", ""),
+        (True, True),
+        (("something",), ("something",)),
+        ({"key": "val"}, {"key": "val"}),
+        ({}, {}),
+        ([], []),
+    ],
+)
+def test_convert_null_values(provided: Any, expected: Any) -> None:
+    """Test convert_null_values."""
+    assert convert_null_values(provided) == expected
+
+
+@pytest.mark.parametrize("provided", [None, Path("./")])
+def test_resolve_path_field(provided: Optional[Path]) -> None:
+    """Test resolve_path_field."""
+    if provided is None:
+        assert not resolve_path_field(provided)
+    else:
+        result = resolve_path_field(provided)
+        assert result.is_absolute()
+        assert result == provided.resolve()
+
+
+@pytest.mark.parametrize(
+    "provided",
+    [
+        None,
+        {"key", "val"},
+        "${env something}",
+        "${var ${env DEPLOY_ENVIRONMENT}.something}",
+        "${something}",  # not desirable but unavoidable with the current regex
+    ],
+)
+def test_validate_string_is_lookup(provided: Any) -> None:
+    """Test validate_string_is_lookup."""
+    assert validate_string_is_lookup(provided) == provided
+
+
+@pytest.mark.parametrize(
+    "provided", ["fail", "${fail", "fail}", "fail.${env fail}", "${env fail}.fail"]
+)
+def test_validate_string_is_lookup_raises(provided) -> None:
+    """Test validate_string_is_lookup."""
+    with pytest.raises(ValueError) as excinfo:
+        validate_string_is_lookup(provided)
+    assert excinfo.value == RUNWAY_LOOKUP_STRING_ERROR
