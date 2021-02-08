5d875fc668e7570ea744e49ea51ddedf4c9e0ddb
Kyle Finley <kyle@finley.sh>
2021-02-08 11:11:27 -0800

    cdk: use pydantic model to parse options (#530)

---
+++ runway/config/models/runway/options/cdk.py
@@ -0,0 +1,21 @@
+"""Runway AWS Cloud Development Kit Module options."""
+from __future__ import annotations
+
+from typing import List
+
+from pydantic import Extra
+
+from ...base import ConfigProperty
+
+
+class RunwayCdkModuleOptionsDataModel(ConfigProperty):
+    """Model for Runway AWS Cloud Development Kit Module options."""
+
+    build_steps: List[str] = []
+    skip_npm_ci: bool = False
+
+    class Config:
+        """Model configuration."""
+
+        extra = Extra.ignore
+        title = "Runway AWS Cloud Development Kit Module options."
