919cb0bf81fd6ae74fa08a1e32188c05502765ea
Kyle Finley <kyle@finley.sh>
2021-02-09 07:41:30 -0800

    k8s: use pydantic model to parse options (#531)

---
+++ runway/config/models/runway/options/k8s.py
@@ -0,0 +1,22 @@
+"""Runway Kubernetes Module options."""
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Optional
+
+from pydantic import Extra
+
+from ...base import ConfigProperty
+
+
+class RunwayK8sModuleOptionsDataModel(ConfigProperty):
+    """Model for Runway Kubernetes Module options."""
+
+    kubectl_version: Optional[str] = None
+    overlay_path: Optional[Path] = None
+
+    class Config:
+        """Model configuration."""
+
+        extra = Extra.ignore
+        title = "Runway Kubernetes Module options."
