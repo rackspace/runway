a5af8c2c98d79dad36398f029ae878f492b7e7c4
Kyle Finley <kyle@finley.sh>
2021-05-26 12:11:20 -0400

    convert cdk codebuild tests to pytest functional tests
    - updated workflow to setup node
    - remove action that was previously added for testing
    - added some ts config files to json formatting in pre-commit

---
+++ tests/functional/cdk/test_multistack/bin/stacks.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { ExampleStack } from "../lib/example-stack";
+
+const app = new cdk.App();
+const namespace: string =
+  app.node.tryGetContext("namespace") || "undefined-namespace";
+
+new ExampleStack(app, (namespace ? namespace + "-" : "") + "multistack-00", {
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { region: "us-east-1" },
+});
+
+new ExampleStack(app, (namespace ? namespace + "-" : "") + "multistack-01", {
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { region: "us-east-1" },
+});
