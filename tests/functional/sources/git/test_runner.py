7a7f1350645b22e00c2fb22b661f484d4fcbdb07
Kyle Finley <kyle@finley.sh>
2021-06-01 15:25:44 -0400

    convert sources codebuild tests to pytest functional tests

---
+++ tests/functional/sources/git/test_runner.py
@@ -0,0 +1,41 @@
+"""Test Runway module from git repo."""
+# pylint: disable=redefined-outer-name
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+from typing import TYPE_CHECKING, Generator
+
+import pytest
+
+from runway._cli import cli
+
+if TYPE_CHECKING:
+    from click.testing import CliRunner, Result
+
+CURRENT_DIR = Path(__file__).parent
+
+
+@pytest.fixture(scope="module")
+def deploy_result(cli_runner: CliRunner) -> Generator[Result, None, None]:
+    """Execute `runway deploy` with `runway destory` as a cleanup step."""
+    yield cli_runner.invoke(cli, ["deploy"], env={"CI": "1"})
+
+
+@pytest.fixture(scope="module")
+def destroy_result(cli_runner: CliRunner) -> Generator[Result, None, None]:
+    """Execute `runway destroy`."""
+    yield cli_runner.invoke(cli, ["destroy"], env={"CI": "1"})
+    shutil.rmtree(CURRENT_DIR / ".runway", ignore_errors=True)
+
+
+@pytest.mark.order("first")
+def test_deploy_exit_code(deploy_result: Result) -> None:
+    """Test deploy exit code."""
+    assert deploy_result.exit_code == 0
+
+
+@pytest.mark.order("last")
+def test_destroy_exit_code(destroy_result: Result) -> None:
+    """Test destory exit code."""
+    assert destroy_result.exit_code == 0
