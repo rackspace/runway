9d0f6e40fd6ac3dd8f4edceb6be89aa8754068a0
Kyle Finley <kyle@finley.sh>
2021-08-03 10:56:53 -0400

    add detail to docstrings

--- runway/cfngin/hooks/protocols.py
+++ runway/cfngin/hooks/protocols.py
@@ -1,4 +1,9 @@
-"""Protocols for structural typing."""
+"""Protocols for structural typing.
+
+For more information on protocols, refer to
+`PEP 544 <https://www.python.org/dev/peps/pep-0544/>`__.
+
+"""
 from __future__ import annotations
 
 from abc import abstractmethod
@@ -11,11 +16,17 @@ if TYPE_CHECKING:
 
 
 class CfnginHookArgsProtocol(Protocol):
-    """Protocol for CFNgin hook arguments class."""
+    """Protocol for CFNgin hook arguments class.
+
+    This class defines a structural interface for all CFNgin hook argument
+    classes. It is recommended to use the provided base class in place of this
+    when authoring a new argument class.
+
+    """
 
     @abstractmethod
     def get(self, name: str, default: Any = None) -> Any:
-        """Implement evaluation of self.get.
+        """Safely get the value of an attribute.
 
         Args:
             name: Attribute name to return the value for.
@@ -29,6 +40,10 @@ class CfnginHookArgsProtocol(Protocol):
         raise NotImplementedError
 
     @abstractmethod
+    def __getattribute__(self, name: str) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
     def __getitem__(self, name: str) -> Any:
         raise NotImplementedError
 
@@ -39,7 +54,14 @@ class CfnginHookArgsProtocol(Protocol):
 
 @runtime_checkable
 class CfnginHookProtocol(Protocol):
-    """Protocol for CFNgin hooks."""
+    """Protocol for CFNgin hooks.
+
+    This class defines a structural interface for all CFNgin hook classes.
+    Classes used for hooks do not need to subclass this hook. They only need to
+    implement a similar interface. While not required, it is still acceptable
+    to subclass this class for full type checking of a hook class.
+
+    """
 
     args: CfnginHookArgsProtocol
 
@@ -47,24 +69,55 @@ class CfnginHookProtocol(Protocol):
     def __init__(  # pylint: disable=super-init-not-called
         self, context: CfnginContext, **_kwargs: Any
     ) -> None:
-        """Structural __init__ method."""
+        """Structural __init__ method.
+
+        This should not be called. Pylint will erroneously warn about
+        "super-init-not-called" if using this class as a subclass. This should
+        be disabled in-line until the bug reports for this issue is resolved.
+
+        """
+        raise NotImplementedError
 
     @abstractmethod
     def post_deploy(self) -> Any:
-        """Run during the **post_deploy** stage."""
+        """Run during the **post_deploy** stage.
+
+        Returns:
+            A "truthy" value if the hook was successful or a "falsy" value if the
+            hook failed.
+
+        """
         raise NotImplementedError
 
     @abstractmethod
     def post_destroy(self) -> Any:
-        """Run during the **post_destroy** stage."""
+        """Run during the **post_destroy** stage.
+
+        Returns:
+            A "truthy" value if the hook was successful or a "falsy" value if the
+            hook failed.
+
+        """
         raise NotImplementedError
 
     @abstractmethod
     def pre_deploy(self) -> Any:
-        """Run during the **pre_deploy** stage."""
+        """Run during the **pre_deploy** stage.
+
+        Returns:
+            A "truthy" value if the hook was successful or a "falsy" value if the
+            hook failed.
+
+        """
         raise NotImplementedError
 
     @abstractmethod
     def pre_destroy(self) -> Any:
-        """Run during the **pre_destroy** stage."""
+        """Run during the **pre_destroy** stage.
+
+        Returns:
+            A "truthy" value if the hook was successful or a "falsy" value if the
+            hook failed.
+
+        """
         raise NotImplementedError
