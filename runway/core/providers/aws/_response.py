a89860be7ae32c32d5b1eb9cd65637f5aaa2c44d
Kyle Finley <kyle@finley.sh>
2022-01-07 12:27:26 -0500

    use `pydantic` for `runway.core.providers.aws._response`

--- runway/core/providers/aws/_response.py
+++ runway/core/providers/aws/_response.py
@@ -1,75 +1,64 @@
 """Base class for AWS responses."""
 # pylint: disable=invalid-name
 from http import HTTPStatus
-from typing import Any, Dict, Optional
+from typing import Any, Dict
+
+from pydantic import Field
+
+from ....utils import BaseModel
 
 
-class ResponseError:
+class ResponseError(BaseModel):
     """Analyse the response from AWS S3 HeadBucket API response.
 
-    Attributes:
-        code: A unique short code representing the error that was emitted.
-        message: A longer human readable error message.
+    Keyword Args:
+        Code: A unique short code representing the error that was emitted.
+        Message: A longer human readable error message.
 
     """
 
-    def __init__(self, *, Code: str = "", Message: str = "") -> None:  # noqa
-        """Instantiate class.
+    code: str = Field(default="", alias="Code")
+    """A unique short code representing the error that was emitted."""
 
-        Args:
-            Code: A unique short code representing the error that was emitted.
-            Message: A longer human readable error message.
-
-        """
-        self.code = Code
-        self.message = Message
+    message: str = Field(default="", alias="Message")
+    """A longer human readable error message."""
 
     def __bool__(self) -> bool:
         """Implement evaluation of instances as a bool."""
         return bool(self.code or self.message)
 
 
-class ResponseMetadata:
+class ResponseMetadata(BaseModel):
     """Analyse the response from AWS S3 HeadBucket API response.
 
-    Attributes:
-        host_id: Host ID data.
-        https_headers: A map of response header keys and
-            their respective values.
-        http_status_code: The HTTP status code of the response (e.g., 200, 404).
-        request_id: The unique request ID associated with the response.
+    Keyword Args:
+        HostId: Host ID data.
+        HTTPHeaders: A map of response header keys and their respective values.
+        HTTPStatusCode: The HTTP status code of the response (e.g., 200, 404).
+        RequestId: The unique request ID associated with the response.
             Log this value when debugging requests for AWS support.
-        retry_attempts: The number of retries that were attempted
-            before the request was completed.
+        RetryAttempts: The number of retries that were attempted before the
+            request was completed.
 
     """
 
-    def __init__(
-        self,
-        *,
-        HostId: Optional[str] = None,  # noqa
-        HTTPHeaders: Optional[Dict[str, Any]] = None,  # noqa
-        HTTPStatusCode: int = 200,  # noqa
-        RequestId: Optional[str] = None,  # noqa
-        RetryAttempts: int = 0,  # noqa
-    ) -> None:
-        """Instantiate class.
+    host_id: str = Field(default="", alias="HostId")
+    """Host ID data."""
 
-        Keyword Args:
-            HostId: Host ID data.
-            HTTPHeaders: A map of response header keys and their respective values.
-            HTTPStatusCode: The HTTP status code of the response (e.g., 200, 404).
-            RequestId: The unique request ID associated with the response.
-                Log this value when debugging requests for AWS support.
-            RetryAttempts: The number of retries that were attempted before the
-                request was completed.
+    https_headers: Dict[str, Any] = Field(default={}, alias="HTTPHeaders")
+    """A map of response header keys and their respective values."""
 
-        """
-        self.host_id = HostId
-        self.https_headers = HTTPHeaders or {}
-        self.http_status_code = HTTPStatusCode
-        self.request_id = RequestId
-        self.retry_attempts = RetryAttempts
+    http_status_code: int = Field(default=200, alias="HTTPStatusCode")
+    """he HTTP status code of the response (e.g., 200, 404)."""
+
+    request_id: str = Field(default="", alias="RequestId")
+    """The unique request ID associated with the response.
+    Log this value when debugging requests for AWS support.
+
+    """
+
+    retry_attempts: int = Field(default=0, alias="RetryAttempts")
+    """The number of retries that were attempted before the request was completed."""
 
     @property
     def forbidden(self) -> bool:
@@ -82,22 +71,19 @@ class ResponseMetadata:
         return self.http_status_code == HTTPStatus.NOT_FOUND
 
 
-class BaseResponse:
+class BaseResponse(BaseModel):
     """Analyse the response from AWS S3 HeadBucket API response.
 
-    Attributes:
-        error: Information about a service or networking error.
-        metadata: Information about the request.
+    Keyword Args:
+        Error: Information about a service or networking error.
+        ResponseMetadata: Information about the request.
 
     """
 
-    def __init__(self, **kwargs: Any) -> None:
-        """Instantiate class.
+    error: ResponseError = Field(default=ResponseError(), alias="Error")
+    """Information about a service or networking error."""
 
-        Keyword Args:
-            Error: Information about a service or networking error.
-            ResponseMetadata: Information about the request.
-
-        """
-        self.error = ResponseError(**kwargs.pop("Error", {}))  # type: ignore
-        self.metadata = ResponseMetadata(**kwargs.pop("ResponseMetadata", {}))  # type: ignore
+    metadata: ResponseMetadata = Field(
+        default=ResponseMetadata(), alias="ResponseMetadata"
+    )
+    """Information about the request."""
