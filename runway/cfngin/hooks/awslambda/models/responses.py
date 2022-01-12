dea0c5c5fee301b670752287fcc6d6c351d031ef
Kyle Finley <kyle@finley.sh>
2022-01-12 12:42:39 -0500

    changed how some pydantic model field aliases are set to better align expectations with type checker behavior (#1151)

--- runway/cfngin/hooks/awslambda/models/responses.py
+++ runway/cfngin/hooks/awslambda/models/responses.py
@@ -1,7 +1,7 @@
 """Response data models."""
 from typing import List, Optional
 
-from pydantic import Extra, Field
+from pydantic import Extra
 
 from runway.utils import BaseModel
 
@@ -17,35 +17,31 @@ class AwsLambdaHookDeployResponse(BaseModel):
 
     """
 
-    bucket_name: str = Field(..., alias="S3Bucket")
+    bucket_name: str
     """Name of the S3 Bucket where the deployment package is located. (alias ``S3Bucket``)"""
 
-    code_sha256: str = Field(..., alias="CodeSha256")
+    code_sha256: str
     """SHA256 of the deployment package.
     This can be used by CloudFormation as the value of ``AWS::Lambda::Version.CodeSha256``.
     (alias ``CodeSha256``)
 
     """
 
-    compatible_architectures: Optional[List[str]] = Field(
-        default=None, alias="CompatibleArchitectures"
-    )
+    compatible_architectures: Optional[List[str]] = None
     """A list of compatible instruction set architectures.
     (https://docs.aws.amazon.com/lambda/latest/dg/foundation-arch.html)
     (alias ``CompatibleArchitectures``)
 
     """
 
-    compatible_runtimes: Optional[List[str]] = Field(
-        default=None, alias="CompatibleRuntimes"
-    )
+    compatible_runtimes: Optional[List[str]] = None
     """A list of compatible function runtimes.
     Used for filtering with ``ListLayers`` and ``ListLayerVersions``.
     (alias ``CompatibleRuntimes``)
 
     """
 
-    license: Optional[str] = Field(default=None, alias="License")
+    license: Optional[str] = None
     """The layer's software license (alias ``License``). Can be any of the following:
 
     - A SPDX license identifier (e.g. ``MIT``).
@@ -55,17 +51,17 @@ class AwsLambdaHookDeployResponse(BaseModel):
 
     """
 
-    object_key: str = Field(..., alias="S3Key")
+    object_key: str
     """Key (file path) of the deployment package S3 Object. (alias ``S3Key``)"""
 
-    object_version_id: Optional[str] = Field(default=None, alias="S3ObjectVersion")
+    object_version_id: Optional[str] = None
     """The version ID of the deployment package S3 Object.
     This will only have a value if the S3 Bucket has versioning enabled.
     (alias ``S3ObjectVersion``)
 
     """
 
-    runtime: str = Field(..., alias="Runtime")
+    runtime: str
     """Runtime of the Lambda Function. (alias ``Runtime``)"""
 
     class Config:
@@ -73,3 +69,13 @@ class AwsLambdaHookDeployResponse(BaseModel):
 
         allow_population_by_field_name = True
         extra = Extra.forbid
+        fields = {
+            "bucket_name": {"alias": "S3Bucket"},
+            "code_sha256": {"alias": "CodeSha256"},
+            "compatible_architectures": {"alias": "CompatibleArchitectures"},
+            "compatible_runtimes": {"alias": "CompatibleRuntimes"},
+            "license": {"alias": "License"},
+            "object_key": {"alias": "S3Key"},
+            "object_version_id": {"alias": "S3ObjectVersion"},
+            "runtime": {"alias": "Runtime"},
+        }
