72b2c31b0ee1a8404aef81dab2bd2d8d72d183a8
Michael Bordash <michael.bordash@mheducation.com>
2023-03-16 16:32:10 -0700

    fix spellcheck

--- tests/unit/cfngin/test_cfngin.py
+++ tests/unit/cfngin/test_cfngin.py
@@ -1,5 +1,5 @@
 """Tests for runway.cfngin entry point."""
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name, unnecessary-dunder-call
 from __future__ import annotations
 
 import shutil
