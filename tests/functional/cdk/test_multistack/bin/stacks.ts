d006cfc5372431f87ee69c100ca04ae67cde663b
Michael Bordash <michael.bordash@rackspace.com>
2024-10-30 14:04:29 -0700

    test_multistack upgrade of CDK to v2

--- tests/functional/cdk/test_multistack/bin/stacks.ts
+++ tests/functional/cdk/test_multistack/bin/stacks.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
+import * as cdk from "aws-cdk-lib";
 import { ExampleStack } from "../lib/example-stack";
 
 const app = new cdk.App();
@@ -8,11 +8,11 @@ const namespace: string =
   app.node.tryGetContext("namespace") || "undefined-namespace";
 
 new ExampleStack(app, (namespace ? namespace + "-" : "") + "multistack-00", {
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  /* For more information, see https://docs.aws.amazon.com/cdk/v2/guide/configure-env.html */
   env: { region: "us-east-1" },
 });
 
 new ExampleStack(app, (namespace ? namespace + "-" : "") + "multistack-01", {
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  /* For more information, see https://docs.aws.amazon.com/cdk/v2/guide/configure-env.html*/
   env: { region: "us-east-1" },
 });
