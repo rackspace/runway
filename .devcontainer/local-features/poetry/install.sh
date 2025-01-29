5d50024a0a42ba36334a98e8d08a1a0bfe97a694
Michael Bordash <michael.bordash@rackspace.com>
2025-01-29 15:28:01 -0500

    remove obsolete settings for poetry upgrade

--- .devcontainer/local-features/poetry/install.sh
+++ .devcontainer/local-features/poetry/install.sh
@@ -14,8 +14,6 @@ function __configure_poetry {
   poetry config cache-dir "${HOME}/.cache-pypoetry"
   poetry config virtualenvs.create true;
   poetry config virtualenvs.in-project true;
-  poetry config virtualenvs.prefer-active-python true;
-  poetry config warnings.export false;
 }
 
 __install_poetry;
